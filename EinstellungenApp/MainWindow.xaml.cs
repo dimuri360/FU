2dd007140c85df02e90b626faed7e08a18bca9e7
dimuri360 <dieter.urich@gmail.com>
2025-06-11 10:26:18 +0200

    Add simple WPF app

---
+++ EinstellungenApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace EinstellungenApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
